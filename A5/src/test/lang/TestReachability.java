66604143eca95a34849315b6c0b72731facaf502
Joaquim Gouveia <joaquimgouveia2000@gmail.com>
2024-10-16 11:32:47 +0200

    removed stuff

--- A5/src/test/lang/TestReachability.java
+++ A5/src/test/lang/TestReachability.java
@@ -8,48 +8,40 @@ import org.junit.runners.Parameterized.Parameters;
 import java.io.PrintStream;
 import java.io.ByteArrayOutputStream;
 
-import lang.ast.ErrorMessage;
-import lang.ast.Program;
+import lang.Util;
+import lang.ast.*;
+
 
 /**
  * This is a parameterized test: one test case is generated for each input
  * file found in TEST_DIRECTORY. Input files should have the ".in" extension.
  */
 @RunWith(Parameterized.class)
-public class TestInterpreter {
+public class TestReachability {
     /** Directory where the test input files are stored. */
-    private static final File TEST_DIRECTORY = new File("testfiles/interpreter");
+    private static final File TEST_DIRECTORY = new File("testfiles/reachability");
 
     private final String filename;
-    public TestInterpreter(String testFile) {
+    public TestReachability(String testFile) {
         filename = testFile;
     }
 
     @Test public void runTest() throws Exception {
         PrintStream out = System.out;
-        try {
             Program program = (Program) Util.parse(new File(TEST_DIRECTORY, filename));
             StringBuilder sb = new StringBuilder();
-            if (program.errors().isEmpty()) {
-                ByteArrayOutputStream baos = new ByteArrayOutputStream();
-                System.setOut(new PrintStream(baos));
-                program.eval();
-                Util.compareOutput(baos.toString(),
-                        new File(TEST_DIRECTORY, Util.changeExtension(filename, ".out")),
-                        new File(TEST_DIRECTORY, Util.changeExtension(filename, ".expected")));
-            } else {
-                for (ErrorMessage m : program.errors()) {
-                    sb.append(m).append("\n");
+            for (FuncDecl f : program.getFuncDeclList()) { 
+                sb.append("Function: " + f.getIdDecl().getID() + "\n");
+                for (FuncDecl fd : f.reachable()){
+                    sb.append(fd.getIdDecl().getID()).append("\n");
                 }
+                sb.append("---------------\n");
+            }
                 String actual = sb.toString();
                 Util.compareOutput(actual,
                         new File(TEST_DIRECTORY, Util.changeExtension(filename, ".out")),
                         new File(TEST_DIRECTORY, Util.changeExtension(filename, ".expected")));
             }
-        } finally {
-            System.setOut(out);
-        }
-    }
 
     @Parameters(name = "{0}")
     public static Iterable<Object[]> getTests() {
